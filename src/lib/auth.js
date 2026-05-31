5a32b036a3d677b6010458040e1d99ac52df1be6
casanovafurtado-ctrl <casanovafurtado@gmail.com>
2026-05-30 23:17:29 -0300

    Add authentication options with Google provider

---
+++ src/lib/auth.js
@@ -0,0 +1,44 @@
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: [
+            'openid',
+            'email', 
+            'profile',
+            'https://www.googleapis.com/auth/calendar',
+            'https://www.googleapis.com/auth/tasks',
+          ].join(' '),
+          access_type: 'offline',
+          prompt: 'consent',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.access_token = account.access_token;
+        token.refresh_token = account.refresh_token;
+        token.expires_at = account.expires_at;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.access_token = token.access_token;
+      session.refresh_token = token.refresh_token;
+      session.error = token.error;
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/',
+    error: '/',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
